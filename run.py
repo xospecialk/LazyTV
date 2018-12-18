2cacb32c076b3ef334ef94cbd043497ccb686850
Kelvin Shek <xospecialk@MacBook-Pro.local>
2018-12-17 20:47:30 -0500

    changed port, added params

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from turnmeon import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=6790)
--- turnmeon/v1/control.py
+++ turnmeon/v1/control.py
@@ -5,13 +5,22 @@ import paramiko
 
 class Control(Resource):
 
+    turn_on = 'echo on 0 | cec-client -s -d 1'
+    turn_off = 'echo standby 0 | cec-client -s -d 1'
+
     def __init__(self):
         self.parser = reqparse.RequestParser()
+        self.parser.add_argument('action')
 
     def get(self):
+        args = self.parser.parse_args()
+        action = args.get('action')
         ssh = paramiko.SSHClient()
         ssh.load_system_host_keys()
         ssh.connect("192.168.0.10", username="root", password="openelec")
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('echo on 0 | cec-client -s -d 1')
+        if action == "on":
+            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(self.turn_on)
+        else:
+            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(self.turn_off)
 
 
